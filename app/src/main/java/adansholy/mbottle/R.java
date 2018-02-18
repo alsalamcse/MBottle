86f21074bdc4e4c632daaf1f1b1d7b200fd0c591
tasnem mobarky <tasnem.mobarky@gmail.com>
2018-02-18 11:36:04 +0200

    [-p

---
+++ app/src/main/java/adansholy/mbottle/R.java
@@ -0,0 +1,8 @@
+package adansholy.mbottle;
+
+/**
+ * Created by user on 18/02/2018.
+ */
+
+interface R {
+}
---
+++ app/src/main/java/adansholy/mbottle/User.java
@@ -0,0 +1,10 @@
+package adansholy.mbottle;
+
+/**
+ * Created by user on 18/02/2018.
+ */
+
+public class User {
+
+
+}
